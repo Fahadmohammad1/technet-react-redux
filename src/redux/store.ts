3f6f2187080754e7da85c95564083feec5c76f79
Fahad <205fahadmohammad@gmail.com>
2023-07-07 23:49:39 +0600

    class 21 done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cartReducer from './features/cart/cartSlice'
+import productReducer from './features/products/productSlice'
+import { api } from './api/apiSlice';
 
 const store = configureStore({
     reducer : {
-        cart : cartReducer
-    }
+        cart : cartReducer,
+        product : productReducer,
+        [api.reducerPath] : api.reducer,
+    },
+    middleware : (getDefauldMiddleware) => getDefauldMiddleware().concat(api.middleware) 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
